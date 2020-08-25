291b982c236334072eb078352500b1701c83ba28
Brendan <brendanerichards@comcast.net>
2020-08-25 14:59:14 -0600

    implemented mutate() and crossover() methods

--- src/Song.py
+++ src/Song.py
@@ -8,11 +8,14 @@ class Song:
 
     def __init__(self, tempo, time_sig, num_bars, parents=()):
         self.tempo = tempo
+        self.fitness = None
         self.time_sig = time_sig # time_sig[0] is the numerator
         self.num_bars = num_bars
         self.note_types = ["whole", "half", "quarter", "eighth"] # "sixteenth", "thirty-second"]
         self.parts = [Part("piano"), Part("guitar")]
         self.chordID = 0 # the next chord to make when creating a song
+        self.mutation_prob = 0.05
+        self.num_mutations = 3
         self.chords = ["Cmaj", "Amin", "Gmaj"]
         self.times = ["half", "half", "whole"]
         self.chord2notes = self.get_chord2notes()
@@ -22,10 +25,11 @@ class Song:
             self.random_song()
         elif len(parents) == 2:
             self.crossover(*parents)
+            self.mutate()
         else:
             print('error, ', len(parents), ' parents were passed to Song constructor.')
             exit(-1)
-        print()
+        self.calc_fitness()
 
     def get_chord2notes(self):
         chord2notes = dict()
@@ -79,13 +83,23 @@ class Song:
 
 
     def crossover(self, p1, p2):
-        None
+        self.parts = p1.parts[:]
+        crosspoint = rand.randint(0, len(p1.parts[0].bars)-1)
+        self.parts[1].bars[crosspoint:] = p2.parts[1].bars[crosspoint:]
 
     def calc_fitness(self):
-        None
+        self.fitness = 1
 
     def mutate(self):
-        None
+        if rand.random() < self.mutation_prob:
+            for _ in range(self.num_mutations):
+                bar_index = rand.randint(0, len(self.parts[1].bars)-1)
+                note_index = rand.randint(0, len(self.parts[1].bars[bar_index])-1)
+                #note_type = self.parts[1].bars[bar_index][note_index].note_type
+                note_num = rand.randint(*self.parts[1].note_range)
+                #note = Note(1.0, note_num, note_type, self.time_sig)
+                self.parts[1].bars[bar_index][note_index].note_num = note_num
+
 
     def play(self):
         None
