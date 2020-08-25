eefa8aaecc459b5301be19bf1bbe2c1e4bdf263f
Brendan <brendanerichards@comcast.net>
2020-08-25 11:17:06 -0600

    added logic for creating notes and creating instrument parts

--- src/Note.py
+++ src/Note.py
@@ -1,5 +1,23 @@
 class Note:
-    def __init__(self, name, velocity, index):
-        self.name = name
+    def __init__(self, velocity, note_num, note_type, time_sig):
         self.velocity = velocity
-        self.index = index
+        self.note_num = note_num
+        self.note_type = note_type
+        self.num_beats = self.get_num_beats(time_sig[1])
+
+    def get_num_beats(self, bottom):
+        if self.note_type == "whole":
+            return bottom
+        elif self.note_type == "half":
+            return bottom/2.0
+        elif self.note_type == "quarter":
+            return bottom/4.0
+        elif self.note_type == "eighth":
+            return bottom/8.0
+        elif self.note_type == "sixteenth":
+            return bottom/16.0
+        elif self.note_type == "thirty-second":
+            return bottom/32.0
+        else:
+            print("unknown note type: ", self.note_type)
+            exit(-1)
--- src/Part.py
+++ src/Part.py
@@ -1,7 +1,15 @@
 class Part:
     def __init__(self, instrument):
         self.instrument = instrument
-        self.range = self.get_range()
+        self.note_range = self.get_range()
+        self.bars = []
 
+    # get the midi note range for the instrument
     def get_range(self):
-        None
+        if self.instrument == "guitar":
+            return (40, 76)
+        elif self.instrument == "piano":
+            return (9, 96)
+        else:
+            print("instrument not recognized")
+            exit(-1)
